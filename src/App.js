edc61380125314e4ad91327f59060dee2baf31ad
dhanvepreeti96 <106668200+dhanvepreeti96@users.noreply.github.com>
2022-09-24 20:33:19 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import Welcome from "./Welcome.js";
+
+function App() {
+  return (
+    <div className="App">
+     <Welcome name="Preeti"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Welcome(prop)
+{
+    return(
+        <>
+        <h1>Hey {prop.name}</h1>
+        <h2>Welcome to Newton School.</h2>
+        </>
+    )
+}
